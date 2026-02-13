6c6851118186b888bdde0c9254ed0997a10eb28a
Alireza <alirezagholinezhaad@gmail.com>
2026-02-14 01:14:38 +0330

    feat: Add Favorites screen to navigation stack

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import PlayerScreen from "../screens/PlayerScreen";
 import TitleScreen from "../screens/TitlteScreen";
+import { FavoriteScreen } from "../screens/FavoriteScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,11 @@ export default function MainNavigator() {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Title" component={TitleScreen} />
       <Stack.Screen name="Player" component={PlayerScreen} />
+      <Stack.Screen
+        name="Favorites"
+        component={FavoriteScreen}
+        options={{ title: "علاقه مندی ها" }}
+      />
     </Stack.Navigator>
   );
 }
